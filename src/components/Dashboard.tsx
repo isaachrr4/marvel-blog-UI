cc98c96f1491c75e5c18460fe60913bf5687be53
isaac harris <isaachrr4@gmail.com>
2022-08-10 15:06:11 -0400

    working on dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,12 +1,23 @@
 import { SyntheticEvent, useState } from "react";
 import { User } from "../models/user";
 import { useNavigate} from "react-router-dom";
+import { Grid } from '@mui/material';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
 
 
 interface IDashboard {
     currentUser: User | undefined
 }
 
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
 
 function Dashboard (props: IDashboard) {
 
@@ -16,6 +27,22 @@ function Dashboard (props: IDashboard) {
         <>
             <h1>
                 Welcome to your Marvel Finder Dashboard!</h1>
+                <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                <Grid item xs={6}>
+                    <Item>
+                        <button onClick={() => navigate('/search')}>Look for a Comic or Character</button>
+                    </Item>
+                        </Grid>
+                        <Grid item xs={6}>
+                        <Item><button onClick={() => navigate('/favorites')}>Look at your favorite comics here</button></Item>
+                            </Grid>
+                    <Grid item xs={6}>
+                        <Item>3</Item>
+                                </Grid>
+                         <Grid item xs={6}>
+                                    <Item>4</Item>
+                                            </Grid>
+                                </Grid> 
 
             
         </>
