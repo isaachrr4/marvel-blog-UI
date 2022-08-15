63e5c1d4dc8462de5a49274d9ce4563755ab3dfb
isaac harris <isaachrr4@gmail.com>
2022-08-15 16:17:22 -0400

    working on favorite comic page

---
+++ src/components/Favoritecomic.tsx
@@ -0,0 +1,17 @@
+import { User } from "../models/user";
+import { SyntheticEvent, useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+import { FavoriteComics} from "../models/favoritecomics";
+
+interface IFavoriteComicProps {
+    userId: number | undefined
+}
+
+function FavoriteComics(props: IFavoriteComicProps) {
+    
+
+}
+
+
+
+export default FavoriteComics;
